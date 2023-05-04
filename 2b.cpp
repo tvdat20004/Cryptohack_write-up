cfecdad6f84c65ae510856cdbe272b45db845790
Thái Vĩnh Đạt <117071011+tvdat20004@users.noreply.github.com>
2023-05-04 21:25:17 +0700

    Update 2b.cpp

--- 2b.cpp
+++ 2b.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+// hàm shift là hàm mã hóa bằng caesar cipher với giá trị shift = 1
 string shift(string &ciphertext)
 {
 	for (int i = 0; i < ciphertext.length(); ++i)
@@ -16,8 +16,10 @@ string shift(string &ciphertext)
 int main()
 {
 	string ciphertext;
+	// nhập ciphertext
 	cout<<"Enter encrypted text:";
 	getline(cin,ciphertext);
+	// thực hiện brute-force với plaintext với các giá trị shift từ 1 tới 26 
 	for (int i = 1; i <= 26; ++i)
 	{
 		cout<<"Shift:"<< i <<"\nPlaintext:"<< shift(ciphertext)<<'\n';
